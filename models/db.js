1f245cc43cd9243b9cf22753f66c9aceb12d66d2
Augusto Caetano Maria <augusto.maria@yara.com>
2022-05-14 15:53:33 -0300

    ajustesBD

--- models/db.js
+++ models/db.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const Sequelize = require('sequelize');
 
-const bd_NAME = process.env.bd_NAME;
-const bd_USER = process.env.bd_USER;
-const bd_PASS = process.env.bd_PASS;
-const bd_HOST = process.env.bd_HOST;
-const bd_DIALECT = process.env.bd_DIALECT;
+const bd_NAME = process.env.database;
+const bd_USER = process.env.user;
+const bd_PASS = process.env.password;
+const bd_HOST = process.env.host;
+const bd_DIALECT = process.env.dialect;
 
-const sequelize = new Sequelize(bd_NAME,bd_USER,bd_PASS, {
+const sequelize = new Sequelize(bd_NAME, bd_USER, bd_PASS, {
     host: bd_HOST,
     dialect: bd_DIALECT,
     pool: {
@@ -24,7 +24,7 @@ sequelize.authenticate()
 .then(() => {
     console.log('Show. Conectou');
 }).catch(() => {
-    console.log('ERRO: Não concectou');
+    console.log(`ERRO: Não concectou`);
 });
 
 module.exports = sequelize
